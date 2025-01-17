57ee2545852949f91e3fa07f78ca2a0781242d91
Apenotis <152433513+Apenotis@users.noreply.github.com>
2025-01-17 13:58:36 +0100

    Update doom.bat

--- doom.bat
+++ doom.bat
@@ -3,12 +3,13 @@
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-SET "gz=Engine\GzDoom\gzdoom.exe"
-SET "PBD=Maps\Mod\PB-0_2_1-alpha.pk3"
-SET "DARK=Maps\Mod\Dark\BDBE_v3.38.pk3 Maps\Mod\Dark\CatsVisorBASE1.10.3.pk3 Maps\Mod\Dark\CatsVisorC1.10.3.pk3"
-SET "Hexen=Maps\Mod\hexen\BrutalHexenRPG_V7.5.pk3"
-SET "Heretic=Maps\Mod\BrutalHereticRPG_V6.1.pk3"
+SET "gz=Engine\GzDoom\gzdoom.exe" //https://zdoom.org/downloads
+SET "PB=Maps\Mod\PB-0_2_1-alpha.pk3" //https://www.moddb.com/mods/project-brutality
+SET "Black=Maps\Mod\Dark\BDBE_v3.38.pk3 Maps\Mod\Dark\CatsVisorBASE1.10.3.pk3 Maps\Mod\Dark\CatsVisorC1.10.3.pk3" //https://www.moddb.com/mods/brutal-doom/addons/brutal-doom-v20b-black-edition
+SET "Hexen=Maps\Mod\hexen\BrutalHexenRPG_V7.5.pk3" //https://www.moddb.com/mods/brutal-hexen
+SET "Heretic=Maps\Mod\BrutalHereticRPG_V6.1.pk3" //https://www.moddb.com/mods/brutal-heretic
 
+REM Map choice
 :menu
 COLOR C
 cls
@@ -17,10 +18,12 @@ echo.
 set /P "M=Wähle eine Karte: "
 cls
 
+REM read CSV
 for /f "usebackq skip=1 tokens=1,* delims=," %%a in ("maps.csv") do (
     set "map[%%a]=%%b"
 )
 
+REM check selection
 IF "%M%"=="0" exit /B
 IF "%M%"=="r" GOTO menu
 if "%M%"=="" goto menu
@@ -30,6 +33,7 @@ if not defined map[%M%] (
     goto menu
 )
 
+REM Set PWAD
 set "mapData=!map[%M%]!"
 set /a index=1
 
@@ -53,6 +57,7 @@ for %%p in (!pwadData!) do (
     )
 )
 
+REM IWAD rename
 set "core=!column[1]!"
 if "!core!"=="maps\iwad\doom.wad" (
     set "displayCore=Doom I"
@@ -68,6 +73,7 @@ if "!core!"=="maps\iwad\doom.wad" (
     set "displayCore=Heretic - Shadow of the Serpent Riders"
 )
 
+REM Mod choice
 COLOR A
 CLS
 echo Wähle einen Mod:
@@ -107,28 +113,30 @@ set /P "modChoice=Wähle Mod:"
 CLS
 timeout /t 1 >nul
 
-rem Debug-Ausgabe
-echo Debug: Map = %mapname%
-echo Debug: IWAD = %displayCore%
-echo Debug: PWAD = %displayFileParams%
+REM Debug
+echo Map  = %mapname%
+echo IWAD = %displayCore%
+echo PWAD =%displayFileParams%
 
+REM launch
 if "%modChoice%"=="1" (
-    echo Mod:  Project Brutality
-    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %PBD% !fileParams!
+    echo Mod  = Project Brutality
+    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %PB% !fileParams!
 ) else if "%modChoice%"=="2" (
-    echo Mod:  Dark Doom
-    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %DARK% !fileParams!
+    echo Mod  = Dark Doom
+    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %Black% !fileParams!
 ) else if "%modChoice%"=="3" (
-    echo Mod:  Brutal Hexen
+    echo Mod  = Brutal Hexen
     "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %HEXEN% !fileParams!
 ) else if "%modChoice%"=="4" (
-    echo Mod:  Brutal Heretic
-    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %HERETIC% !fileParams!
+    echo Mod  = Brutal Heretic
+    "%gz%" +logfile "logfile.txt" -iwad "%core%" -file %HERETIC% !fileParams!   
 ) else if "%modChoice%"=="5" (
-    echo Mod:  Kein Mod ausgewählt
+    echo Mod  = Kein Mod ausgewählt
     "%gz%" +logfile "logfile.txt" -iwad "%core%" -file !fileParams!
 )
 
+REM Memory Clear
 set "mapname="
 set "core="
 set "fileParams="
